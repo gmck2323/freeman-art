0abf9aa97d076d121ab4e2376009bd875cc278eb
gmck2323 <30579200+gmck2323@users.noreply.github.com>
2021-01-03 15:10:14 -0700

    first

---
+++ README.md
@@ -0,0 +1,7 @@
+# Freeman Art Company Landing
+
+
+### <a href="">LIVE</a> 
+
+## Description
+This is the landing page for Freeman Art Company
---
+++ src/components/services.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+export class Services extends Component {
+  render() {
+    return (
+      <div id="services" className="text-center">
+        <div className="container">
+          <div className="section-title">
+            <h2>What We Do</h2>
+            <p>
+              By connecting local businesses with local artists, we equip the community to support each other through art.
+            </p>
+          </div>
+          <div className="row">
+            {this.props.data
+              ? this.props.data.map((d, i) => (
+                  <div  key={`${d.name}-${i}`} className="col-md-4">
+                    {" "}
+                    <i className={d.icon}></i>
+                    <div className="service-desc">
+                      <h3>{d.name}</h3>
+                      <p>{d.text}</p>
+                    </div>
+                  </div>
+                ))
+              : "loading"}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Services;
